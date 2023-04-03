b2b078b5c61818c76437fb725f426894673ead7b
PhaweeGIT <fabrice.tambrun@gmail.com>
2023-04-03 23:42:12 +0200

    EffectUpdate1

--- IAdventure/Assets/Script/Fabrice/TEST.cs
+++ IAdventure/Assets/Script/Fabrice/TEST.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class TEST : MonoBehaviour
 {
-    private void OnBecameInvisible() {
-        print("FUFU");
-    }
+    public GameObject zap;
+    public Transform Enemy;
+    private Transform targetZap;
+    public Transform canon;
+    private GameObject zapSave;
 
-    private void OnBecameVisible() {
-        print("COUCOU");
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.J)) {
+            zapSave = Instantiate(zap, canon.transform.position, Quaternion.identity);
+            targetZap = zapSave.transform.GetChild(0).transform;
+            targetZap.position = Enemy.position + Vector3.up;
+            Destroy(zapSave, 2f);
+        }
     }
 }
