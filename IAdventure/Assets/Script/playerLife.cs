b6b3f8577243904d519d4b01b063c9439cd8bb8c
PhaweeGIT <fabrice.tambrun@gmail.com>
2023-04-03 18:20:29 +0200

    Level-Update7

--- IAdventure/Assets/Script/playerLife.cs
+++ IAdventure/Assets/Script/playerLife.cs
@@ -4,22 +4,46 @@ using UnityEngine;
 
 public class playerLife : MonoBehaviour {
     public int vie = 100;
+    public int armor = 0;
+    private int armorMax;
+    [HideInInspector] public int vieBonus;
     [HideInInspector] public int vieMax;
     private Animator anim;
     private MonoBehaviour[] scripts;
     private bool KO;
+    private bool regenArmor = true;
 
     void Awake() {
         vieMax = vie;
+        armorMax = armor;
         if (TryGetComponent<Animator>(out Animator animator)) {
             anim = animator;
         }
         scripts = GetComponents<MonoBehaviour>();
     }
 
+    void Update() {
+        if (regenArmor && !KO && armorMax > 0 && armor < armorMax) {
+            armor = Mathf.RoundToInt(Mathf.MoveTowards(armor, armorMax, Time.deltaTime * 10f));
+        }
+    }
+
     public void takeDamage(int degats) {
         if (vie > 0) {
-            vie -= degats;
+            if (armor > 0) {
+                armor -= degats;
+                if (armor < 0) {
+                    armor = 0;
+                }
+            }
+            else if (vieBonus > 0) {
+                vieBonus -= degats;
+            } else {
+                vie -= degats;
+            }
+            regenArmor = false;
+            StopCoroutine(armorReset());
+            StartCoroutine(armorReset());
         }
 
         if (vie <= 0 && !KO) {
@@ -49,5 +73,10 @@ public class playerLife : MonoBehaviour {
             }
         }
         vie = Mathf.RoundToInt(vieMax / 2);
+    }
+
+    IEnumerator armorReset() {
+        yield return new WaitForSeconds(3f);
+        regenArmor = true;
     }
 }
